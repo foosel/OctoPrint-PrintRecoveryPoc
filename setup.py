8f3621dd3bd8060fad3b9ac1020488ffc22cb435
Gina Häußge <osd@foosel.net>
2016-02-09 09:39:24 +0100

    We need OctoPrint 1.2.9 at least

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ plugin_url = "https://github.com/foosel/OctoPrint-PrintRecoveryPoc"
 plugin_license = "AGPLv3"
 
 # Any additional requirements besides OctoPrint should be listed here
-plugin_requires = []
+plugin_requires = ["OctoPrint>=1.2.9"]
 
 ### --------------------------------------------------------------------------------------------------------------------
 ### More advanced options that you usually shouldn't have to touch follow after this point
